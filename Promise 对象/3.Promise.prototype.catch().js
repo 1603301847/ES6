7e67294791fb4f480526ef98cf011f0f7e71687f
wy_min <wy_min@westinfosoft.com>
2021-03-02 17:00:07 +0800

    ES6

--- Promise 对象/3.Promise.prototype.catch().js
+++ Promise 对象/3.Promise.prototype.catch().js
@@ -50,3 +50,44 @@ p.then((val) => console.log('fulfilled:', val))
 // 等同于
 p.then((val) => console.log('fulfilled:', val))
     .then(null, (err) => console.log("rejected:", err));
+
+/***
+ * Promise 对象的错误具有“冒泡”性质，会一直向后传递，直到被捕获为止。
+ * 也就是说，错误总是会被下一个catch语句捕获。
+ * 一共有三个 Promise 对象：一个由getJSON()产生，两个由then()产生。
+ * 它们之中任何一个抛出的错误，都会被最后一个catch()捕获。
+ */
+getJSON('/post/1.json').then(function (post) {
+    return getJSON(post.commentURL);
+}).then(function (comments) {
+    // some code
+}).catch(function (error) {
+    // 处理前面三个Promise产生的错误
+});
+
+/**
+ * 一般来说，不要在then()方法里面定义 Reject 状态的回调函数（即then的第二个参数），总是使用catch方法。
+ * 第二种写法要好于第一种写法，理由是第二种写法可以捕获前面then方法执行中的错误，也更接近同步的写法（try/catch）。
+ * 因此，建议总是使用catch()方法，而不使用then()方法的第二个参数。
+ */
+
+// bad
+promise
+    .then(function (data) {
+        // success
+    }, function (err) {
+        // error
+    });
+
+// good
+promise
+    .then(function (data) { //cb
+        // success
+    })
+    .catch(function (err) {
+        // error
+    });
+
+    /**
+     * 跟传统的try/catch代码块不同的是，如果没有使用catch()方法指定错误处理的回调函数，Promise 对象抛出的错误不会传递到外层代码，即不会有任何反应。
+     */
