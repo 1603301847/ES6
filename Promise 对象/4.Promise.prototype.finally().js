26643ac3f64914dab17122ab63b9b3562ab3b319
wy_min <wy_min@westinfosoft.com>
2021-03-03 17:54:28 +0800

    ES6

--- Promise 对象/4.Promise.prototype.finally().js
+++ Promise 对象/4.Promise.prototype.finally().js
@@ -77,3 +77,17 @@ Promise.prototype.finally = function (callback) {
         reason => P.resolve(callback()).then(() => { throw reason })
     );
 };
+
+// finally方法总是会返回原来的值。
+
+// resolve 的值是 undefined
+Promise.resolve(2).then(() => { }, () => { })
+
+// resolve 的值是 2
+Promise.resolve(2).finally(() => { })
+
+// reject 的值是 undefined
+Promise.reject(3).then(() => { }, () => { })
+
+// reject 的值是 3
+Promise.reject(3).finally(() => { })
--- Promise 对象/5.Promise.all().js
+++ Promise 对象/5.Promise.all().js
@@ -1,3 +1,12 @@
 /**
- * 
+ * Promise.all()方法用于将多个 Promise 实例，包装成一个新的 Promise 实例。
+ * Promise.all()方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+ * 另外，Promise.all()方法的参数可以不是数组，但必须具有 Iterator 接口，且返回的每个成员都是 Promise 实例。
+ */
+const p = Promise.all([p1, p2, p3]);
+
+/**
+ * p的状态由p1、p2、p3决定，分成两种情况。
+ * （1）只有p1、p2、p3的状态都变成fulfilled，p的状态才会变成fulfilled，此时p1、p2、p3的返回值组成一个数组，传递给p的回调函数。
+ * （2）只要p1、p2、p3之中有一个被rejected，p的状态就变成rejected，此时第一个被reject的实例的返回值，会传递给p的回调函数。
  */
